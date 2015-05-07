a643fc78a0cc889d36d7ce799515aed8fb953823
Francisco Collao Garate <pcollaog@gmail.com>
2015-05-07 09:05:01 -0300

    CHG: Migrate to junit 4.x

--- velocity-engine-core/src/test/java/org/apache/velocity/test/BlockMacroTestCase.java
+++ velocity-engine-core/src/test/java/org/apache/velocity/test/BlockMacroTestCase.java
@@ -1,142 +1,163 @@
 package org.apache.velocity.test;
 
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.    
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 import org.apache.velocity.runtime.RuntimeConstants;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * This class tests the BlockMacro functionality.
  */
-public class BlockMacroTestCase extends BaseTestCase
+public class BlockMacroTestCase
+    extends AbstractBaseTest
 {
-    public BlockMacroTestCase(String name)
+
+    @Before
+    public void before()
+        throws Exception
     {
-        super(name);
+        super.before();
     }
 
-    public void testMultipleBodyContentIncludes() throws Exception
+    @Test
+    public void testMultipleBodyContentIncludes()
+        throws Exception
     {
         String template = "#macro(foo $txt) Yeah, $txt! $bodyContent $bodyContent#end #@foo(\"woohoo\")jee#end";
         String result = "  Yeah, woohoo! jee jee";
-    
-        assertEvalEquals(result, template);
+
+        assertEvalEquals( result, template );
     }
 
-    public void testNestedVelocityLogic() throws Exception
+    @Test
+    public void testNestedVelocityLogic()
+        throws Exception
     {
         String template = "#macro(foo $txt) Yeah, $txt! $bodyContent#end #@foo(\"woohoo\")#foreach($i in [1..3])$i:#{end}#end";
         String result = "  Yeah, woohoo! 1:2:3:";
-    
-        assertEvalEquals(result, template);
+
+        assertEvalEquals( result, template );
     }
 
-    public void testEmptyBody() throws Exception
+    @Test
+    public void testEmptyBody()
+        throws Exception
     {
         String template = "#macro(foo $txt) Yeah, $txt! $bodyContent#end #@foo(\"woohoo\")#end";
         String result = "  Yeah, woohoo! ";
-    
-        assertEvalEquals(result, template);
+
+        assertEvalEquals( result, template );
     }
 
-    public void testNoArgumentsEmptyBodyCall() throws Exception
+    @Test
+    public void testNoArgumentsEmptyBodyCall()
+        throws Exception
     {
         String template = "#macro(foo) Yeah! $bodyContent#end #@foo()#end";
         String result = "  Yeah! ";
-    
-        assertEvalEquals(result, template);
+
+        assertEvalEquals( result, template );
     }
 
-    public void testCustomBodyReference() throws Exception
+    @Test
+    public void testCustomBodyReference()
+        throws Exception
     {
-        engine.setProperty(RuntimeConstants.VM_BODY_REFERENCE, "myBody");
+        engine.setProperty( RuntimeConstants.VM_BODY_REFERENCE, "myBody" );
         String template = "#macro(foo) Yeah! $myBody#end #@foo()#end";
         String result = "  Yeah! ";
-    
-        assertEvalEquals(result, template);
+
+        assertEvalEquals( result, template );
     }
 
-    public void testVelocity671() throws Exception
+    @Test
+    public void testVelocity671()
+        throws Exception
     {
-        engine.setProperty(RuntimeConstants.VM_PERM_INLINE_LOCAL, Boolean.TRUE);
+        engine.setProperty( RuntimeConstants.VM_PERM_INLINE_LOCAL, Boolean.TRUE );
         String template = "#macro(echo)$bodyContent#end #@echo()Yeah!#end";
         String result = " Yeah!";
-        assertEvalEquals(result, template);
+        assertEvalEquals( result, template );
     }
 
+    @Test
     public void testStrict()
     {
-        engine.setProperty(RuntimeConstants.RUNTIME_REFERENCES_STRICT, true);
-        assertEvalException("#@foo#end");
-        assertEvalException("#@foo()#end");
+        engine.setProperty( RuntimeConstants.RUNTIME_REFERENCES_STRICT, true );
+        assertEvalException( "#@foo#end" );
+        assertEvalException( "#@foo()#end" );
     }
-    
+
+    @Test
     public void testVelocity690()
     {
-        assertEvalEquals(" output ", "#macro(foo) output #end#@foo #end");
-        assertEvalEquals("#[ output )", "#macro(foo2)#[$bodyContent)#end#@foo2 output #end");
-        assertEvalEquals("#[output)", "#macro(foo2)#[$bodyContent)#end#{@foo2}output#end");
-        assertEvalException("#macro(foo) output #end#@foo");
-    }
-    
-    public void testVelocity675() throws Exception
-    {
-      assertEvalEquals("#@foo#end", "#@foo#end"); 
-    }
-    
-    public void testVelocity685() throws Exception
-    {
-        engine.setProperty(RuntimeConstants.VM_ARGUMENTS_STRICT, Boolean.TRUE);
-        assertEvalEquals(" ", "#macro(foo)#end #@foo() junk #end");
+        assertEvalEquals( " output ", "#macro(foo) output #end#@foo #end" );
+        assertEvalEquals( "#[ output )", "#macro(foo2)#[$bodyContent)#end#@foo2 output #end" );
+        assertEvalEquals( "#[output)", "#macro(foo2)#[$bodyContent)#end#{@foo2}output#end" );
+        assertEvalException( "#macro(foo) output #end#@foo" );
     }
 
-    public void testVelocity686() throws Exception
+    @Test
+    public void testVelocity675()
+        throws Exception
     {
-        String template = "#macro(foo)#set( $x = $bodyContent )#end"+
-                          "#@foo()b#end a $x ";
-        assertEvalEquals(" a b ", template);
+        assertEvalEquals( "#@foo#end", "#@foo#end" );
     }
 
+    @Test
+    public void testVelocity685()
+        throws Exception
+    {
+        engine.setProperty( RuntimeConstants.VM_ARGUMENTS_STRICT, Boolean.TRUE );
+        assertEvalEquals( " ", "#macro(foo)#end #@foo() junk #end" );
+    }
+
+    @Test
+    public void testVelocity686()
+        throws Exception
+    {
+        String template = "#macro(foo)#set( $x = $bodyContent )#end" + "#@foo()b#end a $x ";
+        assertEvalEquals( " a b ", template );
+    }
+
+    @Test
     public void testNestedBlockMacro()
     {
-        String template = "#macro(foo)foo:$bodyContent#end"+
-                          "#macro(bar)bar:$bodyContent#end"+
-                          "#@foo()foo,#@bar()bar#end#end";
-        assertEvalEquals("foo:foo,bar:bar", template);
+        String template = "#macro(foo)foo:$bodyContent#end" + "#macro(bar)bar:$bodyContent#end"
+            + "#@foo()foo,#@bar()bar#end#end";
+        assertEvalEquals( "foo:foo,bar:bar", template );
     }
 
+    @Test
     public void testRecursiveBlockMacro()
     {
-        engine.setProperty(RuntimeConstants.VM_MAX_DEPTH, 3);
-        String template = "#macro(foo)start:$bodyContent#end"+
-                          "#@foo()call:$bodyContent#end";
-        assertEvalEquals("start:call:call:call:$bodyContent", template);
+        engine.setProperty( RuntimeConstants.VM_MAX_DEPTH, 3 );
+        String template = "#macro(foo)start:$bodyContent#end" + "#@foo()call:$bodyContent#end";
+        assertEvalEquals( "start:call:call:call:$bodyContent", template );
     }
 
+    @Test
     public void testBlueJoesProblem()
     {
-        engine.setProperty("macro."+RuntimeConstants.PROVIDE_SCOPE_CONTROL, Boolean.TRUE);
-        addTemplate("a", "#macro(wrap $layout)$!macro.put($layout,$bodyContent)#parse($layout)#end"+
-                         "#@wrap('b')a#end");
-        addTemplate("b", "#@wrap('c')b$!macro.get('b')b#end");
-        addTemplate("c", "c$!macro.get('c')c");
-        assertTmplEquals("cbabc", "a");
+        engine.setProperty( "macro." + RuntimeConstants.PROVIDE_SCOPE_CONTROL, Boolean.TRUE );
+        addTemplate( "a", "#macro(wrap $layout)$!macro.put($layout,$bodyContent)#parse($layout)#end"
+            + "#@wrap('b')a#end" );
+        addTemplate( "b", "#@wrap('c')b$!macro.get('b')b#end" );
+        addTemplate( "c", "c$!macro.get('c')c" );
+        assertTmplEquals( "cbabc", "a" );
     }
 }
-
